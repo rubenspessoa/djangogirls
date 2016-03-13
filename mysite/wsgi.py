a86b8c5e56f412e1c72a7411c7af3ed002fbfb5b
RubensPessoa <pessoadebarros@gmail.com>
2016-03-13 14:26:58 -0300

    Setting up Python Anywhere

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/rubenspessoa/djangogirls/'
+
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+application = StaticFilesHandler(get_wsgi_application())
